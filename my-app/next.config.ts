906247028f688d89af988114d45a5ae316781aeb
ShreyaRai20 <shreya200199@gmail.com>
2025-06-20 18:02:49 -0500

    deployment checkup

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ distDir: 'dist',
+ basePath: '/nextjsapp1', // 🔁 this must match your GitHub repo name
+  images: {
+    unoptimized: true,
+  },
+}
+ 
+module.exports = nextConfig
---
+++ my-app/src/app/about/aboutus/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Hey! this is nested route for aboutus for fun.
+    </div>
+  )
+}
+
+export default page
